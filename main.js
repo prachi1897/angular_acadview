c6de22ca7a2d23e2001ec0cbcf8fdf6ffc756347
Prachi <ditprachi@gmail.com>
2017-07-21 11:46:29 +0530

    Todo's list is being displayed

--- main.js
+++ main.js
@@ -2,6 +2,30 @@ var ngtodo=angular.module('ngtodo',[]);
 ngtodo.controller('mainController',function($scope)
 {
 	console.log("controller");
+	$scope.todos=[
+	{
+		title:'Do my homework',
+		details:'I must do my homework'
+	},
+	{
+		title:'Do my Assignment',
+		details:'I have to do my assignment next week'
+	},
+	{
+		title:'Shopping',
+		details:'I have to go for shopping today'
+
+	},
+	{
+		title:'A vacation scheduled',
+		details:'following week'
+	},
+	{
+		title:'A movie',
+		details:'movie tickets booked fot tomorrow'
+	},
+	]
+		
 
 	
 })
